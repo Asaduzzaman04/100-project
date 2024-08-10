d9b676268420d8d40bd38b1891774f62d07b3b2a
Asaduzzaman04 <asadchy152@gmail.com>
2024-08-10 18:42:47 +0600

    weather-app-ui

--- Weather-app/tailwind.config.js
+++ Weather-app/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {},
     fontFamily :{
       "poppins" : ["Poppins", "system-ui"]
+    },
+    boxShadow : {
+      "basic" : "0px 0px 5px #304463"
     }
+   
   },
   plugins: [],
 }
