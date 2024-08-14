79be8d727b85b89ba0f3f5b4eb5a935e39d8d216
Asaduzzaman04 <asadchy152@gmail.com>
2024-08-14 15:20:49 +0600

    password-strength-app-created

--- pass-Strength/script.js
+++ pass-Strength/script.js
@@ -1,13 +1,34 @@
 const passInput = document.getElementById("pass-input");
 const passStr = document.getElementById("pass-chk");
 
-passInput.addEventListener("keyup", ()=>{
-    if(passInput.value.length < 5){
-        passStr.innerText = "your password is to weak"
-         passStr.style.color = `#ff2116`
-    }else if(passInput.value.length > 7 ){
-        passStr.innerText = "strong password"
-        passStr.style.color = `#7bed9f`
+
+passInput.addEventListener("keyup", () => {
+
+  if (passInput.value.length <= 7) {
+
+    passStr.innerText = "your password is to short" ;
+    passStr.style.color = `#ff2116`;
+
+  } else if (passInput.value.length >= 8) {
+
+    if (passInput.value.search(/[a-z]/) == -1) {
+
+      passStr.innerText = "your pass missing short caracter";
+      passStr.style.color = "#ff2116";
+
+    } else if (passInput.value.search(/[A-Z]/) == -1) {
+
+      passStr.innerText = "your pass missing captial caracter";
+      passStr.style.color = "#ff2116";
+
+    }else if(passInput.value.search (/[0-9]/) == -1){
+        passStr.innerText = "your pass missing Numaric caracter";
+        passStr.style.color = "#ff2116";
+    }else{
+        passStr.innerText = "your pass is strong";
+        passStr.style.color = "#27ae60";
     }
-   
-})
+
+  }
+
+});
