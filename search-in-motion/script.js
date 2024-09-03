209ddcc07d7167e2032f8e559c438e05461af60a
Asaduzzaman04 <asadchy152@gmail.com>
2024-09-03 23:54:39 +0600

    serch bar created

---
+++ search-in-motion/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".bx-search");
+const searchBar = document.querySelector(".visible-search")
+btn.addEventListener("click", () => {
+searchBar.style.display = "block"
+})
